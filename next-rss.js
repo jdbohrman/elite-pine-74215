307e1242654564de4e44d260edb0aedffe483728
James Bohrman <jdbohrman@outlook.com>
2021-03-01 21:18:44 -0500

    Add rss

---
+++ next-rss.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteTitle: 'James Bohrman Personal Site',
+    siteDescription: 'Personal Website and Blog of James D. Bohrman',
+    siteLanguage: 'en',
+    siteCopyright: '©James Bohrman',
+    siteUrl: 'http://bohrman.io',
+    outDir: 'public',
+    postsDir: 'posts',
+}
